8af9605c14365a1db719da374b4811e464bd2a02
Feresey <p.milko1999@yandex.ru>
2020-09-05 16:11:28 +0300

    Done lab2-1

--- lab2/lab1/main.go
+++ lab2/lab1/main.go
@@ -11,10 +11,9 @@ func f(x float64) float64 {
 	return math.Pow(3, x) - 5*x*x + 1
 }
 
-// f'(x) = 3^x/10 * ln(3)/sqrt(3^x-1)
-func f_derivatived(x float64) float64 {
-	pow := math.Pow(3, x)
-	return pow / 10 * math.Log(3) / math.Sqrt(pow-1)
+// f'(x) = 3^x * ln(3) - 10*x
+func derived(x float64) float64 {
+	return math.Pow(3, x)*math.Log(3) - 10*x
 }
 
 func fIter(lambda func(float64) float64) func(x float64) float64 {
@@ -25,21 +24,23 @@ func fIter(lambda func(float64) float64) func(x float64) float64 {
 
 func iterations(
 	f func(float64) float64,
+	finish func(iter int, prev, curr float64) bool,
 	from, to float64,
-	q float64,
-	eps float64,
 ) (res float64, iterations int) {
-	fmt.Printf("Iterations\nfrom: %f\nto: %f\n", from, to)
 	var prev, curr float64
 
+	iterations++
 	prev = (from + to) / 2
-	fmt.Println(prev)
+	fmt.Printf("First x_0: %f\n", prev)
 
 	for ; ; iterations++ {
 		curr = f(prev)
-		fmt.Printf("Iter: %d, value: %3.12f\n", iterations, curr)
+		// if math.IsNaN(curr) {
+		// 	panic(prev)
+		// }
 
-		if q/(1-q)*math.Abs(prev-curr) < eps {
+		fmt.Printf("Iter: %d, value: %3.12f\n", iterations, curr)
+		if finish(iterations, prev, curr) {
 			return curr, iterations
 		}
 		prev = curr
@@ -49,11 +50,28 @@ func iterations(
 func main() {
 	from := flag.Float64("from", 0.7, "left point")
 	to := flag.Float64("to", 0.9, "right point")
-	q := flag.Float64("q", 0.25, "magick number, |phi'(x)| <= q < 1, Any x in (a,b)")
+	q := flag.Float64("q", 0.4, "magick number, |phi'(x)| <= q < 1, Any x in (a,b)")
 	lambda := flag.Float64("lambda", -0.1, "second magick number")
 	eps := flag.Float64("eps", 1e-9, "calculation precision")
 	flag.Parse()
 
-	res, iters := iterations(fIter(func(float64) float64 { return *lambda }), *from, *to, *q, *eps)
-	fmt.Printf("Iterations method:\niter: %d\nresult: %3.12f\n", iters, res)
+	fmt.Printf("Iterations\nfrom: %f\nto: %f\n", *from, *to)
+	iterRes, iters := iterations(
+		fIter(func(float64) float64 { return *lambda }),
+		func(iter int, prev, curr float64) bool {
+			return math.Pow(*q, float64(iter))/(1-*q)*math.Abs(prev-curr) < *eps
+		},
+		*from, *to,
+	)
+	fmt.Printf("\nIterations method:\niterations: %d\nresult: %3.12f\n\n", iters, iterRes)
+
+	fmt.Printf("Newthon\nfrom: %f\nto: %f\n", *from, *to)
+	newthonRes, iters := iterations(
+		fIter(func(x float64) float64 { return 1 / derived(x) }),
+		func(iter int, prev, curr float64) bool {
+			return math.Abs(prev-curr) < *eps
+		},
+		*from, *to,
+	)
+	fmt.Printf("\nNewthon method:\niterations: %d\nresult: %3.12f\n\n", iters, newthonRes)
 }
