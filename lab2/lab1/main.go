3eb5c6291402f50587055bc6975f6de34ea2b39a
Feresey <p.milko1999@yandex.ru>
2020-09-05 15:08:28 +0300

    Init for newthon

--- lab2/lab1/main.go
+++ lab2/lab1/main.go
@@ -17,8 +17,10 @@ func f_derivatived(x float64) float64 {
 	return pow / 10 * math.Log(3) / math.Sqrt(pow-1)
 }
 
-func f_iter(x float64) float64 {
-	return x + 0.2*f(x)
+func fIter(lambda func(float64) float64) func(x float64) float64 {
+	return func(x float64) float64 {
+		return x - lambda(x)*f(x)
+	}
 }
 
 func iterations(
@@ -35,7 +37,7 @@ func iterations(
 
 	for ; ; iterations++ {
 		curr = f(prev)
-		fmt.Printf("Iter: %d, value: %f\n", iterations, curr)
+		fmt.Printf("Iter: %d, value: %3.12f\n", iterations, curr)
 
 		if q/(1-q)*math.Abs(prev-curr) < eps {
 			return curr, iterations
@@ -48,9 +50,10 @@ func main() {
 	from := flag.Float64("from", 0.7, "left point")
 	to := flag.Float64("to", 0.9, "right point")
 	q := flag.Float64("q", 0.25, "magick number, |phi'(x)| <= q < 1, Any x in (a,b)")
+	lambda := flag.Float64("lambda", -0.1, "second magick number")
 	eps := flag.Float64("eps", 1e-9, "calculation precision")
 	flag.Parse()
 
-	res, iters := iterations(f_iter, *from, *to, *q, *eps)
+	res, iters := iterations(fIter(func(float64) float64 { return *lambda }), *from, *to, *q, *eps)
 	fmt.Printf("Iterations method:\niter: %d\nresult: %3.12f\n", iters, res)
 }
