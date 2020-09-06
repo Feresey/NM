b5a35ddc4d7e2c10e7704e60051093839420f8d8
Feresey <p.milko1999@yandex.ru>
2020-09-06 21:07:33 +0300

    Done iterations

--- lab2/lab2/main.go
+++ lab2/lab2/main.go
@@ -38,6 +38,14 @@ func detJ(x1, x2 float64) float64 {
 	return df1X1(x1, x2)*df2X2(x1, x2) - df2X1(x1, x2)*df1X2(x1, x2)
 }
 
+func phi1(x1, x2 float64) float64 {
+	return 3 + math.Cos(x2)
+}
+
+func phi2(x1, x2 float64) float64 {
+	return 3 + math.Sin(x2)
+}
+
 func newthon(x1, x2 float64, eps float64) (res1 float64, res2 float64, iterations int) {
 	var (
 		prevX1 = x1
@@ -64,14 +72,47 @@ func newthon(x1, x2 float64, eps float64) (res1 float64, res2 float64, iteration
 	}
 }
 
+func iterations(x1, x2 float64, q float64, eps float64) (res1 float64, res2 float64, iterations int) {
+	var (
+		prevX1 = x1
+		prevX2 = x2
+	)
+
+	iterations++
+	for ; ; iterations++ {
+		x1 = phi1(x1, x2)
+		x2 = phi2(x2, x1)
+
+		diff1 := math.Abs(prevX1 - x1)
+		diff2 := math.Abs(prevX2 - x2)
+
+		fmt.Printf("iteration: %d\n", iterations)
+		fmt.Printf("x1: %3.15f, diff: %3.15f\n", x1, diff1)
+		fmt.Printf("x2: %3.15f, diff: %3.15f\n", x2, diff2)
+
+		max := diff1
+		if diff2 > max {
+			max = diff2
+		}
+
+		if math.Pow(q, float64(iterations))/(1-q)*max < eps {
+			return x1, x2, iterations
+		}
+
+		prevX1, prevX2 = x1, x2
+	}
+}
+
 func main() {
 	x1 := flag.Float64("x1", 4, "first point")
 	x2 := flag.Float64("x2", 4, "second point")
 	eps := flag.Float64("eps", 1e-9, "calculation accuracy")
-	// q := flag.Float64("q", 0.4, "magick number, |phi'(x)| <= q < 1, Any x in (a,b)")
-	// lambda := flag.Float64("lambda", -0.1, "second magick number")
+	q := flag.Float64("q", 0.4, "magick number, |phi'(x)| <= q < 1, Any x in (a,b)")
 	flag.Parse()
 
+	x1Iter, x2Iter, totalIter := iterations(*x1, *x2, *q, *eps)
+	fmt.Printf("\nIterations\niterations: %d\nx1=%3.15f,x2=%3.15f\n", totalIter, x1Iter, x2Iter)
+
 	x1Newthon, x2Newthon, totalNewthon := newthon(*x1, *x2, *eps)
-	fmt.Printf("\nnewthon. iterations: %d\nx1=%3.15f,x2=%3.15f\n", totalNewthon, x1Newthon, x2Newthon)
+	fmt.Printf("\nNewthon\niterations: %d\nx1=%3.15f,x2=%3.15f\n", totalNewthon, x1Newthon, x2Newthon)
 }
