797664e64d2f71a2eac3beb7ccedb2879779c981
Feresey <p.milko1999@yandex.ru>
2020-09-11 16:39:33 +0300

    Easy lagrange

---
+++ lab2/lab3/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type points struct {
+	x, y []float64
+}
+
+func Lagrange(points points) func(float64) float64 {
+	w := make([]float64, len(points.x))
+	for idx := range points.x {
+		var res float64 = 1
+		for curr, point := range points.x {
+			if curr != idx {
+				res *= points.x[idx] - point
+			}
+		}
+		w[idx] = res
+	}
+
+	return func(x float64) float64 {
+		var res float64
+		var W float64 = 1
+		for i := range points.x {
+			W *= x - points.x[i]
+		}
+
+		for i := range w {
+			ww := points.y[i] * W / (x - points.x[i]) / w[i]
+			res += ww
+		}
+		return res
+	}
+}
+
+// n
+// x1, x2, ..., xn
+// y1, y2, ..., yn
+// x*
+func main() {
+	var (
+		n      int
+		points points
+		x      float64
+	)
+	fmt.Scanln(&n)
+	points.x = make([]float64, n)
+	points.y = make([]float64, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&points.x[i])
+	}
+	for i := 0; i < n; i++ {
+		fmt.Scan(&points.y[i])
+	}
+	fmt.Scanln(&x)
+
+	f := Lagrange(points)
+
+	fmt.Printf("res: %3.15f\n", f(x))
+}
---
+++ lab2/lab3/main_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func makePoints(x []float64, f func(float64) float64) (res points) {
+	res.x = make([]float64, len(x))
+	res.y = make([]float64, len(x))
+	for i, p := range x {
+		res.x[i] = p
+		res.y[i] = f(p)
+	}
+	return res
+}
+
+func TestLagrange(t *testing.T) {
+	tests := []struct {
+		name   string
+		points points
+		x      float64
+		want   float64
+		delta  float64
+	}{
+		{
+			name:   "ln",
+			points: makePoints([]float64{0.1, 0.5, 0.9, 1.3}, math.Log),
+			x:      0.8,
+			want:   math.Log(0.8),
+			delta:  0.025,
+		},
+		{
+			name: "sin",
+			points: makePoints([]float64{0, 1, 2, 3}, func(x float64) float64 {
+				return math.Sin(math.Pi / 6 * x)
+			}),
+			x:     1.5,
+			want:  math.Sin(math.Pi / 6 * 1.5),
+			delta: 0.0165,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			f := Lagrange(tt.points)
+
+			require.InDelta(t, tt.want, f(tt.x), tt.delta)
+		})
+	}
+}
